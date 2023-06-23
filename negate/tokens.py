b30b7ec6b32d875fcc6b267cc2fde06611ce0d08
Diego Miguel <dmlls@diegomiguel.me>
2023-06-23 20:24:20 +0200

    Add docstrings to all `Negator` methods

--- negate/tokens.py
+++ negate/tokens.py
@@ -9,7 +9,7 @@ class Token:
 
     A :obj:`spacy.tokens.Token` is read-only. See
     `https://spacy.io/api/token`__. Furthermore, it cannot hold an empty string
-    as :attr:`text`. This class is just a minimalist equivalent to a Spacy Token
+    as :attr:`text`. This class is just a minimal equivalent to a spaCy token
     that is both writable and allows empty strings as text. It only contains the
     attributes relevant for our purposes.
 
@@ -19,7 +19,7 @@ class Token:
         has_space_after (:obj:`str`):
             Whether the token is followed by a whitespace (this whitespace is
             not part of the :attr:`text`). This information is used for
-            detokenization.
+            de-tokenization.
     """
 
     text: str
