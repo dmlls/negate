c44cd3f6fb457043a9532d8fed2b92bfd39e88ce
Diego Miguel <dmlls@diegomiguel.me>
2024-05-03 16:06:39 +0200

    Update spaCy models' versions

--- negate/version.py
+++ negate/version.py
@@ -4,5 +4,5 @@
 __version__ = "1.1.3"
 
 # spaCy models version.
-EN_CORE_WEB_MD_VERSION: str = "3.7.0"
-EN_CORE_WEB_TRF_VERSION: str = "3.7.2"
+EN_CORE_WEB_MD_VERSION: str = "3.7.1"
+EN_CORE_WEB_TRF_VERSION: str = "3.7.3"
