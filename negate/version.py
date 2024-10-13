0665e25e0b1fc2fd1b37a93073fc1f0e4b511a20
Diego Miguel <dmlls@diegomiguel.me>
2024-10-13 14:18:34 +0200

    Update dependency versions

--- negate/version.py
+++ negate/version.py
@@ -1,8 +1,8 @@
 """Version specification."""
 
 # Negate version.
-__version__ = "1.1.5"
+__version__ = "1.1.6"
 
 # spaCy models version.
-EN_CORE_WEB_MD_VERSION: str = "3.7.1"
-EN_CORE_WEB_TRF_VERSION: str = "3.7.3"
+EN_CORE_WEB_MD_VERSION: str = "3.8.0"
+EN_CORE_WEB_TRF_VERSION: str = "3.8.0"
